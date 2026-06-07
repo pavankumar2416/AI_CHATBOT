10be0131d46c38c15d29e0f66addce0e355decb5
pavankumar2416 <pavankumar76821@gmail.com>
2026-06-08 00:47:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,10 +11,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
-const app = express();
 
-app.use(cors());
-app.use(express.json());
 
 // =========================
 // CONFIG
